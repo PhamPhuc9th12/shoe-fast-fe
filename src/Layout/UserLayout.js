1261d3eef8ddc2f0c9513b2aaa0e9354b0a4ebb2
PhamPhuc09 <pphuc9122002@gmail.com>
2024-10-05 15:54:24 +0700

    add Footer.js

--- src/Layout/UserLayout.js
+++ src/Layout/UserLayout.js
@@ -2,17 +2,16 @@ import React, { useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 import { Switch, Route } from "react-router-dom";
 import Header from "../common/Header";
+import Footer from "../common/Footer";
 const UserLayout = () =>{
 
     return(
         <div className="col-10 offset-1">
-            <Header>
-
-            </Header>
+            <Header></Header>
             <Switch>
                 
             </Switch>
-            {/* <Footer></Footer> */}
+            <Footer></Footer>
             <ToastContainer></ToastContainer>           
         </div>
     );
