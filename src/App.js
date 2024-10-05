5d7abd0e5ca6e9ec3ac87c3a582df641af49429e
PhamPhuc09 <pphuc9122002@gmail.com>
2024-10-05 12:20:26 +0700

    init frame

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-
+import UserLayOut from "./Layout/UserLayout";
+import { BrowserRouter as Router } from "react-router-dom";
 function App() {
   return (
     <div className="App">
@@ -18,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <Router>
+        <UserLayOut></UserLayOut> 
+      </Router>
     </div>
   );
 }
---
+++ src/Layout/UserLayout.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import { ToastContainer } from "react-toastify";
+import { Switch, Route } from "react-router-dom";
+const UserLayout = () =>{
+
+    return(
+        <div className="col">
+            {/* <Header>
+
+            </Header> */}
+            <Switch>
+                
+            </Switch>
+            {/* <Footer></Footer> */}
+            <ToastContainer></ToastContainer>           
+        </div>
+    );
+}
+export default UserLayout;
