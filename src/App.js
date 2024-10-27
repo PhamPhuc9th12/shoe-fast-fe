606e04dd8588228eae35b04527417c621fb27cc4
PhamPhuc09 <pphuc9122002@gmail.com>
2024-10-27 22:24:18 +0700

    [FE] : admin- mere api synthesys + UI dashboard admin

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import UserLayOut from "./layout/UserLayout";
 import { BrowserRouter as Router } from "react-router-dom";
+import AdminLayOut from './layout/AdminLayout';
 function App() {
   return (
     <div className="container-fluid">
       <Router>
         <UserLayOut></UserLayOut>
+        <AdminLayOut></AdminLayOut>
       </Router>
     </div>
   );
---
+++ src/Layout/AdminLayout.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "../assets/boxicons-2.0.7/css/boxicons.min.css";
+import "../assets/css/grid.css";
+import "../assets/css/index.css";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import '../static/css/style.css';
+import Account from "../component/admin/Account";
+import DashboardAdmin from "../component/admin/dashboard/DashboardAdmin";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const AdminLayOut = () => {
+
+    const [user, setUser] = useState(null);
+
+    const userHandler = (user) => {
+        setUser(user);
+    };
+
+    return (
+        <>
+            <Router path="/account" exact>
+                <Account user={user} userHandler={userHandler}></Account>
+                <ToastContainer></ToastContainer>
+            </Router>
+            <Router path="/admin" exact>
+                <DashboardAdmin ></DashboardAdmin>
+            </Router>
+        </>
+    );
+};
+
+export default AdminLayOut;
