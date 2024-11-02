9139f9e7eed875ff6dd601969b76504625900d8f
PhamPhuc09 <pphuc9122002@gmail.com>
2024-11-02 09:27:18 +0700

    [FE] : UI+API create new brand

--- src/api/BrandApi.js
+++ src/api/BrandApi.js
@@ -3,3 +3,16 @@ export const getBrands = (page, size) => {
     const url = `/api/v1/brand/list?page=${page}&size=${size}`;
     return Instance.get(url);
 }
+export const createBrand = (data) => {
+    const url = `/api/v1/brand/create`;
+    return Instance.post(url, data);
+}
+
+export const getBrandDetail = (id) => {
+    const url = `/api/v1/brand/detail/${id}`;
+    return Instance.get(url);
+}
+export const updateBrand = (data) => {
+    const url = `/api/v1/brand/update`;
+    return Instance.post(url, data);
+}
