70def0f5b57ac8cf70522cf6906b2755581547a4
PhamPhuc09 <pphuc9122002@gmail.com>
2024-11-03 22:06:10 +0700

    [FE] UI + API update, create, get detail sale

--- src/api/SaleApi.js
+++ src/api/SaleApi.js
@@ -4,3 +4,17 @@ export const getSale = (page, size) => {
     const url = `/api/v1/sale/list?page=${page}&size=${size}`;
     return Instance.get(url);
 }
+
+export const createSale = (data) => {
+    const url = `/api/v1/sale/create`;
+    return Instance.post(url, data);
+}
+
+export const getSaleDetail = (id) => {
+    const url = `/api/v1/sale/detail/${id}`;
+    return Instance.get(url);
+}
+export const updateSale = (data) => {
+    const url = `/api/v1/sale/update`;
+    return Instance.post(url, data);
+}
