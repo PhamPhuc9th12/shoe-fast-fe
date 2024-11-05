87da5ce184d75237c11f52b2f2be257aab5ddbcc
PhamPhuc09 <pphuc9122002@gmail.com>
2024-11-05 22:30:51 +0700

    [FE] : UI+ merge api get list voucher for admin

--- src/api/VoucherApi.js
+++ src/api/VoucherApi.js
@@ -4,3 +4,16 @@ export const getVoucherByCode = (code) => {
     const url = `/api/v1/voucher/by-code?code=${code}`;
     return Instance.get(url);
 }
+
+export const getVouchers = (page, size) => {
+    const url = `/api/v1/voucher/list?page=${page}&size=${size}`;
+    return Instance.get(url);
+}
+export const createVoucher = (data) => {
+    const url = `/api/v1/voucher/create`;
+    return Instance.post(url, data);
+}
+export const getVoucherDetail = (id) => {
+    const url = `/api/v1/voucher/detail/${id}`;
+    return Instance.get(url);
+}
