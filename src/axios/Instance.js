6e883bf3e474d8961dc699a9508fb9f2d0850928
PhamPhuc09 <pphuc9122002@gmail.com>
2024-11-17 12:00:59 +0700

    [FE]: change localhost to domain

--- src/axios/Instance.js
+++ src/axios/Instance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const Instance = axios.create({
-    baseURL: "http://localhost:8086",
-    // baseURL: "https://25fd-116-101-91-180.ngrok-free.app",
+    // baseURL: "http://localhost:8086",
+    baseURL: "https://shoe-fast-be-1.onrender.com",
     headers: {
         "Content-Type": "application/json"
     }
